4f3ca845bb7e26f59c2240b3cce856b46df684cf
SekiCokeZERO <92011506+SekiCokeZERO@users.noreply.github.com>
2024-03-14 09:56:22 +0800

    更新

--- ContractCharm.sol
+++ ContractCharm.sol
@@ -1,27 +1,32 @@
 //SPDX-License-Identifier: UNLICENSED
-pragma solidity =0.8.22;
+pragma solidity =0.8.24;
 
 //编译时请检查 compiler_config.json 中的 evmVersion 以避免造成兼容性问题
 contract Factory {
-    address immutable _owner;
+    address immutable _owner = msg.sender;
     bytes constant _loaderBytecode =
-        hex"608080604052346100e9576352c7420d60e01b81526000908181600481335afa9182156100dd578092610035575b825160208401f35b9091503d8082843e61004681610104565b8083019260208092126100d95780516001600160401b03918282116100d55785609f830112156100d5578101519182116100d05760405194610091601f8401601f191685018761012a565b82865260a083830101116100cc5782845b8381106100b75750505083010152388061002d565b60a081840101518282890101520183906100a2565b8380fd5b6100ee565b8480fd5b8280fd5b604051903d90823e3d90fd5b600080fd5b634e487b7160e01b600052604160045260246000fd5b6080601f91909101601f19168101906001600160401b038211908210176100d057604052565b601f909101601f19168101906001600160401b038211908210176100d05760405256fe";
+        hex"608080604052346100ea576352c7420d60e01b81526000908181600481335afa9182156100de578092610035575b825160208401f35b3d9250908282823e61004683610105565b8281019260208091126100da5781516001600160401b03928382116100d65785609f830112156100d6578101519283116100d15760405194610092601f8501601f19166020018761012b565b83865260a084830101116100cd57835b8381106100b957505050820160200152388061002d565b81810160a0015186820184015282016100a2565b8380fd5b6100ef565b8480fd5b8280fd5b604051903d90823e3d90fd5b600080fd5b634e487b7160e01b600052604160045260246000fd5b6080601f91909101601f19168101906001600160401b038211908210176100d157604052565b601f909101601f19168101906001600160401b038211908210176100d15760405256fe";
+    bytes private _bytecode = hex"01";
 
-    bytes private _bytecode;
+    //必须传入无 constructor 合约的 Runtime Bytecode, 否则所创建的合约将不可用; 欲创建的合约必须预留 selfdestruct, 否则后续将无法进行升级
+    function setBytecode(bytes memory bytecode) external {
+        require(msg.sender == _owner, "PERMISSION_DENIED");
+        _bytecode = bytecode;
+    }
 
-    constructor() {
-        _owner = msg.sender;
+    function getBytecode() external view returns (bytes memory) {
+        return _bytecode;
     }
 
     function deploy(uint256 salt) external {
         require(msg.sender == _owner, "PERMISSION_DENIED");
-        require(_bytecode.length > 0, "EMPTY_BYTECODE");
+        require(_bytecode.length > 1, "EMPTY_BYTECODE");
         require(
-            _isNoCode(_calculateAddress(_loaderBytecode, salt)),
+            _isNoCode(_calculateCreate2Address(_loaderBytecode, salt)),
             "ALREADY_DEPLOYED"
         );
 
-        bytes memory loaderBytecode = _loaderBytecode; //内联汇编不支持直接调用常量
+        bytes memory loaderBytecode = _loaderBytecode;
         assembly {
             pop(
                 create2(
@@ -34,17 +39,7 @@ contract Factory {
         }
     }
 
-    //必须传入无 constructor 合约的 Runtime Bytecode, 否则所创建的合约将不可用; 欲创建的合约必须预留 selfdestruct, 否则后续将无法进行升级
-    function setBytecode(bytes memory bytecode) external {
-        require(msg.sender == _owner, "PERMISSION_DENIED");
-        _bytecode = bytecode;
-    }
-
-    function getBytecode() external view returns (bytes memory) {
-        return _bytecode;
-    }
-
-    function _calculateAddress(bytes memory bytecode, uint256 salt)
+    function _calculateCreate2Address(bytes memory bytecode, uint256 salt)
         internal
         view
         returns (address)
@@ -61,11 +56,11 @@ contract Factory {
     }
 
     function _isNoCode(address account) internal view returns (bool) {
-        uint256 size;
+        uint256 codeSize;
         assembly {
-            size := extcodesize(account)
+            codeSize := extcodesize(account)
         }
-        return size == 0;
+        return codeSize == 0;
     }
 }
 
--- RunRemix.bat
+++
@@ -1,2 +0,0 @@
-start https://remix.ethereum.org/
-remixd -s %~dp0 --remix-ide https://remix.ethereum.org
